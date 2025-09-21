d27606be41b7a70469371de40f0232ec4c7bbbd2
yehvaed <20567435+yehvaed@users.noreply.github.com>
2025-09-21 20:45:32 +0200

    feat(flake): autoload apps and hosts

--- default.nix
+++ default.nix
@@ -4,6 +4,7 @@ let
 
   perSystem =
     { config
+    , self'
     , pkgs
     , system
     , ...
@@ -31,32 +32,7 @@ let
     imports = [
       inputs.nix-config-modules.flakeModule
       inputs.git-hooks.flakeModule
-
-      # ==> apps
-      ./nix/apps/devbox.nix
-      ./nix/apps/direnv.nix
-      ./nix/apps/distrobox.nix
-      ./nix/apps/eza.nix
-      ./nix/apps/fd.nix
-      ./nix/apps/fonts.nix
-      ./nix/apps/fzf.nix
-      ./nix/apps/gh.nix
-      ./nix/apps/git.nix
-      ./nix/apps/nix-index.nix
-      ./nix/apps/nixos-cli.nix
-      ./nix/apps/nzf.nix
-      ./nix/apps/podman.nix
-      ./nix/apps/ripgrep.nix
-      ./nix/apps/sudo.nix
-      ./nix/apps/tmux.nix
-      ./nix/apps/nvf
-      # ./nix/apps.nix
-
-      ./nix/defaultTags.nix
-
-      # ==> hosts
-      ./nix/hosts/blackout
-      # ./nix/hosts/hosts.nix
+      ./nix/autoload.nix
     ];
 
     nix-config.homeApps = [ ];
